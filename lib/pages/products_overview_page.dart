f719cc7690d9678442a816eef7abf8094ba51525
Marcos <marcos.aucorrea@gmail.com>
2022-12-04 20:06:51 -0300

    App Shop

---
+++ lib/pages/products_overview_page.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/components/app_drawer.dart';
+import 'package:shop/components/badge.dart';
+import 'package:shop/components/product_grid.dart';
+import 'package:shop/models/cart.dart';
+import 'package:shop/utils/app_routes.dart';
+
+enum FilterOptions {
+  favorite,
+  all,
+}
+
+class ProductOverViewPage extends StatefulWidget {
+  const ProductOverViewPage({super.key});
+
+  @override
+  State<ProductOverViewPage> createState() => _ProductOverViewPageState();
+}
+
+class _ProductOverViewPageState extends State<ProductOverViewPage> {
+  bool _showFavorityOnly = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Minha Loja'),
+        actions: [
+          PopupMenuButton(
+            icon: const Icon(Icons.more_vert),
+            itemBuilder: (_) => [
+              const PopupMenuItem(
+                  value: FilterOptions.favorite,
+                  child: Text('Somente Favoritos')),
+              const PopupMenuItem(
+                  value: FilterOptions.all, child: Text('Todos'))
+            ],
+            onSelected: (FilterOptions selectedValue) {
+              setState(() {
+                if (selectedValue == FilterOptions.favorite) {
+                  _showFavorityOnly = true;
+                } else {
+                  _showFavorityOnly = false;
+                }
+              });
+            },
+          ),
+          Consumer<Cart>(
+            builder: (context, cart, child) => Badge(
+              value: cart.itemsCount.toString(),
+              child: child!,
+            ),
+            child: IconButton(
+              onPressed: () {
+                Navigator.of(context).pushNamed(AppRoutes.cart);
+              },
+              icon: const Icon(Icons.shopping_cart),
+            ),
+          ),
+        ],
+      ),
+      body: ProductGrid(_showFavorityOnly),
+      drawer: const AppDrawer(),
+    );
+  }
+}
